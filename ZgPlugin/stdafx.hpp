159664c83e2342b380c5a6c47a5b4199cea82d83
Kurisu <robin@kurisumaki.se>
2023-10-08 17:37:26 -0700

    Update stdafx.hpp

--- ZgPlugin/stdafx.hpp
+++ ZgPlugin/stdafx.hpp
@@ -1,9 +1,10 @@
 #pragma once
-
-#include "./SDK/SdkImport.hpp"
-using namespace Zeitgeist::SdkImport;
+#include "./Sdk/SdkImport.hpp"
+// NOLINT(clang-diagnostic-header-hygiene)
+using namespace Zeitgeist::SdkImport;  
 
 #include "./Helpers/Spell.h"
-#include "./Champions/Katarina.h"
 #include "./Helpers/Utils.h"
+#include "./Champions/Katarina.h"
+#include "./Champions/Camille.h"
 
