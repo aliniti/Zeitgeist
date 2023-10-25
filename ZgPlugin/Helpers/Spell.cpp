bdadc68fb89999c2212d6eebd9c5d96bcad551df
Kurisu <robin@kurisumaki.se>
2023-10-24 23:52:41 -0700

    Spell.h & Spell.cpp

--- ZgPlugin/Helpers/Spell.cpp
+++ ZgPlugin/Helpers/Spell.cpp
@@ -1,5 +1,6 @@
 #include "../stdafx.hpp"
 
+// Constructor
 Spell::Spell( SpellSlot slot )
 {
     this->slot = slot;
@@ -7,6 +8,7 @@ Spell::Spell( SpellSlot slot )
     this->range = 999999.f;
 }
 
+// Constructor
 Spell::Spell( SpellSlot slot, float range )
 {
     this->slot = slot;
@@ -14,6 +16,85 @@ Spell::Spell( SpellSlot slot, float range )
     this->time = 0;
 }
 
+// Get the starting position
+auto Spell::HitBox( ) const -> bool
+{
+    return this->hitbox;
+}
+
+// Get the spell range
+auto Spell::Range( ) const -> float
+{
+    return this->range;
+}
+
+// Get the spell radius
+auto Spell::Radius( ) const -> float
+{
+    return this->radius;
+}
+
+// Get the spell level
+auto Spell::Level( ) const -> int
+{
+    return GetPlayer(  )->Spellbook(  )->GetSpell( this->slot )->Level(  );
+}
+
+// Get the spell slot
+auto Spell::Slot( ) const -> SpellSlot
+{
+    return this->slot;
+}
+
+// Get the last cast time
+auto Spell::LastCastTime( ) const -> float
+{
+    return this->time;
+}
+
+// Get the spell hash
+auto Spell::Hash( ) const -> uint32_t
+{
+    return GetPlayer(  )->Spellbook(  )->GetSpell( this->slot )->SpellData(  )->Hash(  );
+}
+
+// Set the starting position
+void Spell::SetFrom( Vector3 pos )
+{
+    this->from = pos;
+}
+
+// Set the range
+auto Spell::SetRange( float range ) -> void
+{
+    this->range = range;
+}
+
+// Set the delay
+auto Spell::SetDelay( float delay ) -> void
+{
+    this->delay = delay;
+}
+
+// Set the speed
+auto Spell::SetSpeed( float speed ) -> void
+{
+    this->speed = speed;
+}
+
+// Set the radius
+auto Spell::SetRadius( float radius ) -> void
+{
+    this->radius = radius;
+}
+
+// Set the hitbox
+auto Spell::SetHitBox( bool hitbox ) -> void
+{
+    this->hitbox = hitbox;
+}
+
+// Set the skill values
 auto Spell::SetSkillShot( float delay, float speed, float radius, bool hitbox, PredictionCollisionFlags flags ) -> void
 {
     this->delay = delay;
@@ -23,118 +104,7 @@ auto Spell::SetSkillShot( float delay, float speed, float radius, bool hitbox, P
     this->flags = flags;
 }
 
-auto Spell::Range( ) const -> float
-{
-    return this->range;
-}
-
-auto Spell::Radius( ) const -> float
-{
-    return this->radius;
-}
-
-auto Spell::Level( ) const -> int
-{
-    return GetPlayer(  )->Spellbook(  )->GetSpell( this->slot )->Level(  );
-}
-
-auto Spell::Slot( ) const -> SpellSlot
-{
-    return this->slot;
-}
-
-auto Spell::LastCastTime( ) const -> float
-{
-    return this->time;
-}
-
-auto Spell::Hash( ) const -> uint32_t
-{
-    return GetPlayer(  )->Spellbook(  )->GetSpell( this->slot )->SpellData(  )->Hash(  );
-}
-
-auto Spell::IsReady( const float t ) const -> bool
-{
-    const auto spellbook = GetPlayer( )->Spellbook( );
-    if ( !spellbook ) return false;
-
-    const auto spell = spellbook->GetSpell( this->slot );
-    if ( !spell || spell->Level( ) <= 0 ) return false;
-
-    if ( spell->GetRemainingCooldown( ) > 0 && spell->GetRemainingCooldown( ) <= t ) return true;
-    return spellbook->CanUseSpell( this->slot );
-}
-
-auto Spell::Cast( ) -> void
-{
-    if ( g_pExportedGlobalClocks->GameTime( ) - this->time <= .25f )
-        return;
-    
-    const auto player = GetPlayer( );
-    if ( const auto spellbook = player->Spellbook( ) )
-    {
-        auto position = player->Position( );
-        this->time = g_pExportedGlobalClocks->GameTime( );
-        spellbook->SendSpellCastPacket( this->slot, &position, &position, nullptr );
-    }
-}
-
-auto Spell::Cast( GameObject* target ) -> void
-{
-    if ( g_pExportedGlobalClocks->GameTime( ) - this->time <= .25f )
-        return;
-    
-    const auto player = GetPlayer( );
-    if ( const auto spellbook = player->Spellbook( ) )
-    {
-        auto position = target->Position( );
-        this->time = g_pExportedGlobalClocks->GameTime( );
-        spellbook->SendSpellCastPacket( this->slot, &position, &position, target );
-    }
-}
-
-auto Spell::Cast( Vector3 position ) -> void
-{
-    if ( g_pExportedGlobalClocks->GameTime( ) - this->time <= .25f )
-        return;
-    
-    const auto player = GetPlayer( );
-    if ( const auto spellbook = player->Spellbook( ) )
-    {
-        this->time = g_pExportedGlobalClocks->GameTime( );
-        spellbook->SendSpellCastPacket( this->slot, &position, &position, nullptr );
-    }
-}
-
-void Spell::SetFrom( Vector3 pos )
-{
-    this->from = pos;
-}
-
-auto Spell::SetRange( float range ) -> void
-{
-    this->range = range;
-}
-
-auto Spell::SetDelay( float delay ) -> void
-{
-    this->delay = delay;
-}
-auto Spell::SetSpeed( float speed ) -> void
-{
-    this->speed = speed;
-}
-
-auto Spell::SetRadius( float radius ) -> void
-{
-    this->radius = radius;
-}
-
-auto Spell::SetHitBox( bool hitbox ) -> void
-{
-    this->hitbox = hitbox;
-}
-
+// Run prediction from the player
 auto Spell::RunPrediction( GameObject* unit, PredictionOutput& output ) const -> bool
 {
     if ( !unit ) return false;
@@ -154,6 +124,7 @@ auto Spell::RunPrediction( GameObject* unit, PredictionOutput& output ) const ->
     return g_pExportedPrediction->GetPrediction( unit, input, output );
 }
 
+// Run prediction from a given position
 auto Spell::RunPrediction( GameObject* from, GameObject* unit, PredictionOutput& output ) const -> bool
 {
     if ( !unit ) return false;
@@ -174,7 +145,60 @@ auto Spell::RunPrediction( GameObject* from, GameObject* unit, PredictionOutput&
     return g_pExportedPrediction->GetPrediction( unit, input, output );
 }
 
-auto Spell::HitBox( ) -> bool
+// Cast the spell
+auto Spell::Cast( ) -> void
 {
-    return this->hitbox;
+    if ( g_pExportedGlobalClocks->GameTime( ) - this->time <= .25f )
+        return;
+    
+    const auto player = GetPlayer( );
+    if ( const auto spellbook = player->Spellbook( ) )
+    {
+        auto position = player->Position( );
+        this->time = g_pExportedGlobalClocks->GameTime( );
+        spellbook->SendSpellCastPacket( this->slot, &position, &position, nullptr );
+    }
 }
+
+// Cast the spell on the target
+auto Spell::Cast( GameObject* target ) -> void
+{
+    if ( g_pExportedGlobalClocks->GameTime( ) - this->time <= .25f )
+        return;
+    
+    const auto player = GetPlayer( );
+    if ( const auto spellbook = player->Spellbook( ) )
+    {
+        auto position = target->Position( );
+        this->time = g_pExportedGlobalClocks->GameTime( );
+        spellbook->SendSpellCastPacket( this->slot, &position, &position, target );
+    }
+}
+
+// Cast the spell at the given position
+auto Spell::Cast( Vector3 position ) -> void
+{
+    if ( g_pExportedGlobalClocks->GameTime( ) - this->time <= .25f )
+        return;
+    
+    const auto player = GetPlayer( );
+    if ( const auto spellbook = player->Spellbook( ) )
+    {
+        this->time = g_pExportedGlobalClocks->GameTime( );
+        spellbook->SendSpellCastPacket( this->slot, &position, &position, nullptr );
+    }
+}
+
+// Check if the spell is ready to be casted
+auto Spell::IsReady( const float t ) const -> bool
+{
+    const auto spellbook = GetPlayer( )->Spellbook( );
+    if ( !spellbook ) return false;
+
+    const auto spell = spellbook->GetSpell( this->slot );
+    if ( !spell || spell->Level( ) <= 0 ) return false;
+
+    if ( spell->GetRemainingCooldown( ) > 0 && spell->GetRemainingCooldown( ) <= t ) return true;
+    return spellbook->CanUseSpell( this->slot );
+}
+
--- ZgPlugin/Helpers/Spell.h
+++ ZgPlugin/Helpers/Spell.h
@@ -3,15 +3,16 @@
 class Spell
 {
 public:
-    Spell( SpellSlot slot );
+    explicit Spell( SpellSlot slot );
     Spell( SpellSlot slot, float range );
     
-    auto SetSkillShot( float delay, float speed, float radius, bool hitbox, PredictionCollisionFlags flags ) -> void;
-    auto IsReady( float t = 0 ) const -> bool;
-
-    auto Cast( ) -> void;
-    auto Cast( GameObject* target ) -> void;
-    auto Cast( Vector3 position ) -> void;
+    auto HitBox( ) const -> bool;
+    auto Range( ) const -> float;
+    auto Radius( ) const -> float;
+    auto Level( ) const -> int;
+    auto Slot( ) const -> SpellSlot;
+    auto LastCastTime( ) const -> float;
+    auto Hash( ) const -> uint32_t;
     
     auto SetFrom( Vector3 pos ) -> void;
     auto SetRange( float range ) -> void;
@@ -20,17 +21,15 @@ public:
     auto SetRadius( float radius ) -> void;
     auto SetHitBox( bool hitbox ) -> void;
 
+    auto SetSkillShot( float delay, float speed, float radius, bool hitbox, PredictionCollisionFlags flags ) -> void;
     auto RunPrediction( GameObject* unit, PredictionOutput& output ) const -> bool;
     auto RunPrediction( GameObject* from, GameObject* unit, PredictionOutput& output ) const -> bool;
-
-    auto HitBox( ) -> bool;
-    auto Range( ) const -> float;
-    auto Radius( ) const -> float;
-    auto Level( ) const -> int;
-    auto Slot( ) const -> SpellSlot;
-    auto LastCastTime( ) const -> float;
-    auto Hash( ) const -> uint32_t;
-
+    
+    auto Cast( ) -> void;
+    auto Cast( GameObject* target ) -> void;
+    auto Cast( Vector3 position ) -> void;
+    auto IsReady( float t = 0 ) const -> bool;
+    
     auto operator new( size_t size ) -> void* { return Globals::Malloc( size ); }
     auto operator delete( void* p ) noexcept -> void { Globals::Free( p ); }
 
